c132cb0b75e7477da17eb67e307711515412afe6
PERATHURU CHANDRAVAMSI <vamsiperathuru@gmail.com>
2022-03-21 20:00:54 +0530

    changes

--- src/CarouselHome.js
+++ src/CarouselHome.js
@@ -10,10 +10,14 @@ import headphones from "./images/headphones.jpg";
 
 import homeb2 from "./images/homeb2.jpg";
 import homeb3 from "./images/homeb3.jpg";
-import homeb4 from "./images/homeb4.jpg";
+import homeb4 from "./images/homeb4.png";
 import homeb5 from "./images/homeb5.jpg";
+
 import homeb6 from "./images/homeb6.jpg";
 import homeb7 from "./images/homeb7.jpg";
+import homeb8 from "./images/homeb8.jpg";
+import homeb9 from "./images/homeb9.jpg";
+
 
 import { Link } from "react-router-dom";
 import "./Navlinks.css";
@@ -32,9 +36,16 @@ function CarouselHome(){
                <img className="carousel_image"  src={saharahome} alt="h"/>
               {show?<Link className="V_button_Home" to="/Sahara">Go to Sahara Page </Link>:null}
           </button>
-          <div className="carousel_image" onClick={() => {window.location.href="https://amzn.to/3hHDsCC"}}>
-             <img className="carousel_image"  onClick={() => {window.location.href="https://amzn.to/3hHDsCC"}} src={homeb5}  alt="h"/>
+          <div className="carousel_image" onClick={() => {window.location.href="https://amzn.to/3wo4wiF"}}>
+             <img className="carousel_image"  onClick={() => {window.location.href="https://amzn.to/3wo4wiF"}} src={homeb7}  alt="h"/>
            </div>
+           <div className="carousel_image" onClick={() => {window.location.href="https://amzn.to/34Xgu7y"}}>
+             <img className="carousel_image"  onClick={() => {window.location.href="https://amzn.to/34Xgu7y"}} src={homeb8}  alt="h"/>
+           </div>
+          <div className="carousel_image" onClick={() => {window.location.href="https://amzn.to/3Jsfzek"}}>
+             <img className="carousel_image"  onClick={() => {window.location.href="https://amzn.to/3Jsfzek"}} src={homeb5}  alt="h"/>
+           </div>
+          
            <div className="carousel_image" onClick={() => {window.location.href="https://amzn.to/3IIWiVJ"}}>
              <img className="carousel_image"  onClick={() => {window.location.href="https://amzn.to/3IIWiVJ"}} src={homeb6}  alt="h"/>
            </div>
@@ -43,18 +54,22 @@ function CarouselHome(){
            <div className="carousel_image" onClick={() => {window.location.href="https://amzn.to/3vuEYA0"}}>
              <img className="carousel_image" onClick={() => {window.location.href="https://amzn.to/3vuEYA0"}} src={homeb2} alt="h"/>
            </div>
-           <div className="carousel_image" onClick={() => {window.location.href="https://amzn.to/3Mt0Odn"}}>
-             <img className="carousel_image" onClick={() => {window.location.href="https://amzn.to/3Mt0Odn"}} src={homeb7} alt="h"/>
+           
+           <div className="carousel_image" onClick={() => {window.location.href="https://amzn.to/3CVKgpW"}}>
+             <img className="carousel_image" onClick={() => {window.location.href="https://amzn.to/3CVKgpW"}} src={homeb4} alt="h"/>
            </div>
+
            <div className="carousel_image" onClick={() => {window.location.href="https://amzn.to/35vNJix"}}>
              <img className="carousel_image" onClick={() => {window.location.href="https://amzn.to/35vNJix"}} src={homeb3} alt="h"/>
            </div>
            <div className="carousel_image" onClick={() => {window.location.href="https://amzn.to/34c527S"}}>
               <img className="carousel_image" onClick={() => {window.location.href="https://amzn.to/34c527S"}} src={headphones} alt="h"/>
            </div>
-           <div className="carousel_image" onClick={() => {window.location.href="https://amzn.to/3tsV7TR"}}>
-              <img className="carousel_image" onClick={() => {window.location.href="https://amzn.to/3tsV7TR"}} src={homeb4} alt="h"/>
+           <div className="carousel_image" onClick={() => {window.location.href="https://amzn.to/3L53tbF"}}>
+              <img className="carousel_image" onClick={() => {window.location.href="https://amzn.to/3L53tbF"}} src={homeb9} alt="h"/>
            </div>
+          
+           
        </AliceCarousel>
     </div>
     );
